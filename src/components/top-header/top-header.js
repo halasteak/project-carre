d3fbd05f9cafa68205221ac353a71847fdf0dcf1
halasteak <alexandru.capustean@gmail.com>
2020-01-24 02:01:43 +0200

    feat: add phone and mail icons add email click event to send email on click

--- src/components/top-header/top-header.js
+++ src/components/top-header/top-header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./top-header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhoneAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const TopHeader = () => {
   return (
@@ -7,11 +9,19 @@ const TopHeader = () => {
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
           <span className="nav-link">
-            <a href="tel:+33752230282">0752230282</a>
+            <a href="tel:+33752230282">
+              <FontAwesomeIcon icon={faPhoneAlt} size="2x" color="white" />
+              <span> 0752230282</span>
+            </a>
           </span>
         </li>
         <li className="nav-item">
-          <span className="nav-link">projetcarre2020@gmail.com</span>
+          <span className="nav-link">
+            <a href="mailTo:projetcarre2020@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" color="white" />
+              <span> projetcarre2020@gmail.com</span>
+            </a>
+          </span>
         </li>
       </ul>
     </nav>
