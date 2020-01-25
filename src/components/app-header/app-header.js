c02c415f5ba753a04f602be69e8a58b3c439bb28
halasteak <alexandru.capustean@gmail.com>
2020-01-25 12:50:55 +0200

    feat: add toggle menu

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,65 +1,56 @@
 import React, { Component } from "react";
 import "./app-header.css";
 import logo from "../../img/logo.png";
+import {
+  Button,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Container
+} from "react-bootstrap";
 
 class AppHeader extends Component {
-  state = {
-    isOpen: false
-  };
+  // state = {
+  //   isOpen: false
+  // };
 
-  toggleCollapse = () => {
-    console.log("toggleCollapse");
-    this.setState({ isOpen: !this.state.isOpen });
-  };
+  // toggleCollapse = () => {
+  //   console.log("toggleCollapse");
+  //   this.setState({ isOpen: !this.state.isOpen });
+  // };
 
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-light bg-light mb-3">
-        <div className="container">
-          <div className="navbar-brand-section align-items-center">
-            <img src={logo} className="col"></img>
-            <a className="navbar-brand col" href="#">
-              <span>Project Carré</span>
-            </a>
-          </div>
-          <button
-            className="navbar-toggler"
-            data-toggle="collapse"
-            data-target="#navbarNav"
+      <Container>
+        <Navbar bg="red" expand="sm" className="navbar-light">
+          <Navbar.Brand
+            href="#home"
+            className="navbar-brand-section align-items-center"
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  accueil
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  domaines d'activite
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  portfolio
-                </a>
-              </li>
-              {/* <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contact
-                </a>
-              </li> */}
-            </ul>
-          </div>
-        </div>
-      </nav>
+            <img src={logo} className="col"></img>
+            <br />
+            <Navbar.Text>Project Carré</Navbar.Text>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto nav-items-link">
+              <Nav.Link href="#home">accueil</Nav.Link>
+              <Nav.Link href="#domaines_d_activite">
+                domaines d'activite
+              </Nav.Link>
+              <Nav.Link href="#portfolio">portfolio</Nav.Link>
+              <Nav.Link href="#portfolio">portfolio</Nav.Link>
+              <Nav.Link href="#contact">contact</Nav.Link>
+            </Nav>
+            {/* <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
     );
   }
 }
