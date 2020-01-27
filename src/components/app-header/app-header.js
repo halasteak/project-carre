f167f9e2928fc17c2d167abebee27705162500af
halasteak <alexandru.capustean@gmail.com>
2020-01-28 00:58:51 +0200

    feat: add icons svg fixed hover on the menu item resize page using flex for top and nav components

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./app-header.css";
 import logo from "../../img/logo.png";
+import searchIcon from "../../img/searchIcon.svg"
 import {
   Button,
   Navbar,
@@ -10,6 +11,8 @@ import {
   FormControl,
   Container
 } from "react-bootstrap";
+
+const navBarToggled = true;
 
 class AppHeader extends Component {
   // state = {
@@ -23,34 +26,44 @@ class AppHeader extends Component {
 
   render() {
     return (
-      <Container>
-        <Navbar bg="red" expand="sm" className="navbar-light">
-          <Navbar.Brand
-            href="#home"
-            className="navbar-brand-section align-items-center"
-          >
-            <img src={logo} className="col"></img>
-            <br />
-            <Navbar.Text>Project Carré</Navbar.Text>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto nav-items-link">
-              <Nav.Link href="#home">accueil</Nav.Link>
-              <Nav.Link href="#domaines_d_activite">
-                domaines d'activite
-              </Nav.Link>
-              <Nav.Link href="#portfolio">portfolio</Nav.Link>
-              <Nav.Link href="#portfolio">portfolio</Nav.Link>
-              <Nav.Link href="#contact">contact</Nav.Link>
-            </Nav>
-            {/* <Form inline>
+      <div className="row">
+        <div className="col-1"></div>
+        <div className=" col-10">
+          {/* <Container> */}
+            <Navbar expand="sm">
+              <Navbar.Brand
+                href="#home"
+                className="navbar-brand-section align-items-center"
+              >
+                <img src={logo} className="col"></img>
+                <br />
+                <Navbar.Text>Project Carré</Navbar.Text>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse
+                id="basic-navbar-nav  d-sm-block "
+                in={navBarToggled}
+              >
+                <Nav className="ml-auto nav-items-link">
+                  <Nav.Link href="#home">accueil</Nav.Link>
+                  <Nav.Link href="#domaines_d_activite">
+                    domaines d'activite
+                  </Nav.Link>
+                  <Nav.Link href="#portfolio">services</Nav.Link>
+                  <Nav.Link href="#portfolio">portfolio</Nav.Link>
+                  <Nav.Link href="#contact">contact</Nav.Link>
+                </Nav>
+                <embed src={searchIcon} className="ml-3"/>
+                {/* <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-success">Search</Button>
           </Form> */}
-          </Navbar.Collapse>
-        </Navbar>
-      </Container>
+              </Navbar.Collapse>
+            </Navbar>
+          {/* </Container> */}
+        </div>
+        <div className="col-1"></div>
+      </div>
     );
   }
 }
--- src/components/top-header/top-header.js
+++ src/components/top-header/top-header.js
@@ -1,40 +1,36 @@
 import React from "react";
 import "./top-header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import phoneIcon from "../../img/phoneIcon.svg";
+import emailIcon from "../../img/emailIcon.svg";
 import { faPhoneAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const TopHeader = () => {
   return (
     <nav className="navbar navbar-expand  d-none d-sm-block top-header">
-      <div className="container">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <span className="nav-link">
-              <a href="tel:+33752230282">
-                <FontAwesomeIcon
-                  icon={faPhoneAlt}
-                  size="2x"
-                  color="white"
-                  className="nav-icon"
-                />
-                <span> 0752230282</span>
-              </a>
-            </span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-link">
-              <a href="mailTo:projetcarre2020@gmail.com">
-                <FontAwesomeIcon
-                  icon={faEnvelope}
-                  size="2x"
-                  color="white"
-                  className="nav-icon"
-                />
-                <span> projetcarre2020@gmail.com</span>
-              </a>
-            </span>
-          </li>
-        </ul>
+      <div className="row">
+        <div className="col-1"></div>
+        <div className=" col-9">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <span className="nav-link">
+                <a href="tel:+33752230282">
+                  <embed src={phoneIcon}  className="nav-icon"/>
+                  <span> 0752230282</span>
+                </a>
+              </span>
+            </li>
+            <li className="nav-item">
+              <span className="nav-link">
+                <a href="mailTo:projetcarre2020@gmail.com">
+                  <embed src={emailIcon}  className="nav-icon"/>
+                  <span> projetcarre2020@gmail.com</span>
+                </a>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-2"></div>
       </div>
     </nav>
   );
