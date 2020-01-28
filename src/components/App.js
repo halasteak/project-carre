ac6ee66a60abcb9f645c4aaba7ca0e59c774bea3
halasteak <alexandru.capustean@gmail.com>
2020-01-28 23:40:53 +0200

    feat: add ga

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,19 @@ import React, { Fragment } from "react";
 import TopHeader from "./top-header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppHeader from "./app-header";
+import ReactGA from "react-ga";
+// import auth from "./auth.ts"; // Sample authentication provider
+
+const trackingId = "UA-157135052-1";
+ReactGA.initialize(trackingId);
+// ReactGA.set({
+//   userId: auth.currentUserId()
+// });
 
 const App = () => {
   return (
     <Fragment>
-    <TopHeader></TopHeader>
+      <TopHeader></TopHeader>
       <AppHeader></AppHeader>
     </Fragment>
   );
