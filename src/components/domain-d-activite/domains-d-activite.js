fd4ae9e79438bea7bd0838c1593f30fef83afba7
halasteak <alexandru.capustean@gmail.com>
2020-02-07 01:56:29 +0200

    feat: add second block comercial in domains

--- src/components/domain-d-activite/domains-d-activite.js
+++ src/components/domain-d-activite/domains-d-activite.js
@@ -4,6 +4,7 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import ResidentielImg from "./../../img/domains/ResidentielImg.svg";
+import ComercialImg from "./../../img/domains/CommercialImg.svg";
 
 class DomainDActivite extends Component {
   state = {};
@@ -11,28 +12,28 @@ class DomainDActivite extends Component {
     return (
       <div className=" domains-d-activite">
         <div className="row domains-header">
-          <div className="col-2"></div>
-          <div className="col-8">
+          <div className="col-md-2 col-lg-3"></div>
+          <div className="col-md-8 col-lg-6">
             <div className="row content">
-              <div className="container p-4">
+              <div className="container p-3">
                 <h1 className="">
                   <span>Domaines D'activite</span>
                 </h1>
               </div>
             </div>
           </div>
-          <div className="col-2"></div>
+          <div className="col-md-2 col-lg-4"></div>
         </div>
 
-        <section id="explore-section" className="bg-light text-muted py-5">
+        <section id="explore-section" className="bg-light py-5">
           <div className="container">
             <div className="row">
-              <div className="col-md-7">
-                <img src={ResidentielImg} alt="" className="img-fluid mb-3" />>
+              <div className="col-md-6">
+                <img src={ResidentielImg} alt="" className="img-fluid mb-3" />
               </div>
-              <div className="col-md-5">
+              <div className="col-md-6">
                 <h3 className="text-center">Résidentiel</h3>
-                <p>
+                <span>
                   Quelque soit le type de rénovation ou de construction, Projet
                   Carré propose des services de design permetant la réalisation
                   de lieux personnalisés tout en demeurant accessibles à tous
@@ -42,7 +43,33 @@ class DomainDActivite extends Component {
                   l'affût de ces tendences qui vont au-delà des models. Projet
                   Carré vous accompagne tout au long du processus, de la
                   conception au choix des matériaux et rendu final.
-                </p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="explore-section" className="bg-light py-5">
+          <div className="container">
+            <div className="row ">
+              <div className="col-md-6 float-sm-right">
+                <h3 className="text-center">Commercial</h3>
+                <span>
+                  L'élaboration d'un espace commercial, quelle que soit sa
+                  nature, est un projet qui exige efficacité, professionnalisme
+                  mais aussi créativité pour que votre espace se démarque de la
+                  concurrence. Projet Carré, avec son approche personnalisée,
+                  conçoit des lieux commerciaux novateurs et contemporains. Nous
+                  nous efforçons de créer un espace qui fera vivre une
+                  expérience unique à vos clients, simplifiera le travail de vos
+                  emplyés et véhiculera une image positive de votre entreprise.
+                  Nous comprenons l'importance de la mission et de la culture de
+                  votre entreprise, et nous travaillons en ce sens pour en
+                  réliser le reflet le plus fidèle.
+                </span>
+              </div>
+              <div className="col-md-6 float-sm-left">
+                <img src={ComercialImg} alt="" className="img-fluid mb-3" />
               </div>
             </div>
           </div>
